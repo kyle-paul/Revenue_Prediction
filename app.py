a017f3071a0f936e3426eb0c9ac359d73c8485ba
Kyle Paul <kylepaul1501@gmail.com>
2023-03-15 01:18:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ st.title("REVENUE PREDICTION")
 x = st.number_input('Input temperature')
 
 if st.button('Predict'):
+    import pickle
     model = pickle.load(open(filename, "rb"))
     x_new = x.reshape(-1, 1)
     y_new = model.predict(x_new)
