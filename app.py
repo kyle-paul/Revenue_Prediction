d1d2d8d7b56b543321f1101631012e3ee6bf8919
Kyle Paul <kylepaul1501@gmail.com>
2023-03-15 01:19:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ x = st.number_input('Input temperature')
 
 if st.button('Predict'):
     import pickle
+    filename = 'model.pickle'
     model = pickle.load(open(filename, "rb"))
     x_new = x.reshape(-1, 1)
     y_new = model.predict(x_new)
