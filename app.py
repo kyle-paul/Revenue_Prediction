2dc01cbb831a9aedcdd17ce05d6d147ef0e11ec3
Kyle Paul <kylepaul1501@gmail.com>
2023-03-25 00:25:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import numpy as np
+import pickle
 
 st.title("REVENUE PREDICTION")
 x = st.number_input('Input temperature')
 x_new = np.array([x])
 
 if st.button('Predict'):
-    import pickle
-    filename = 'model.pickle'
+    st.caption('Predict Revenue')
     model = pickle.load(open('model.pickle', 'rb'))
     x_new = x_new.reshape(-1,1)
     y_new = model.predict(x_new)
